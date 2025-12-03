720c906ceb7d04bfb0f28b37a7d0918c5a3bd63c
brahim <92426196+BrhmDev@users.noreply.github.com>
2025-12-03 07:29:23 +0000

    add scheduler error handling

--- Scheduler.py
+++ Scheduler.py
@@ -1,11 +1,12 @@
 import time
 from typing import Callable
-
-class Task:
-    session = None,
+import logging
 
 
 def schedule_interval(func: Callable, interval_minutes: int):
     while True:
-        func()
+        try:
+            func()
+        except Exception as e:
+            logging.error(f"Exception thrown while executing task: {e}")
         time.sleep(interval_minutes * 60)
