328dcd3676bc5be3a93149ca55721ffd4e20ea6b
brahim <92426196+BrhmDev@users.noreply.github.com>
2025-12-03 07:29:23 +0000

    add example data

--- create-db.py
+++ create-db.py
@@ -8,8 +8,10 @@ DBSession.query(Record).delete()
 
 
 DBSession.add(Record(row_id=1, organisation_id="org1", local_id="/file", md5_checksum="0284bb853a649751efbca489e6132b12", md5_checksum_partner="0284bb853a649751efbca489e6132b12"))
-DBSession.add(Record(row_id=2, organisation_id="org1", local_id="/file", md5_checksum="abc", md5_checksum_partner=None))
-DBSession.add(Record(row_id=3, organisation_id="org2", local_id="/file", md5_checksum="efg", md5_checksum_partner=None))
-DBSession.add(Record(row_id=4, organisation_id="org3", local_id="/file", md5_checksum="lll", md5_checksum_partner=None))
+DBSession.add(Record(row_id=2, organisation_id="org1", local_id="/file", md5_checksum="valid_checksum", md5_checksum_partner="valid_checksum"))
+DBSession.add(Record(row_id=3, organisation_id="org2", local_id="/missing_partner_checksum", md5_checksum="efg", md5_checksum_partner=None))
+DBSession.add(Record(row_id=4, organisation_id="org2", local_id="/missing_partner_checksum2", md5_checksum="efg", md5_checksum_partner=None))
+DBSession.add(Record(row_id=5, organisation_id="NotAPartner1", local_id="/not_a_partner", md5_checksum="valid_checksum", md5_checksum_partner="valid_checksum"))
+DBSession.add(Record(row_id=6, organisation_id="org4", local_id="/hash_not_in_mediahaven", md5_checksum="hash_not_in_mediahaven", md5_checksum_partner="hash_not_in_mediahaven"))
 
 DBSession.commit()
